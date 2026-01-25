ef46671c5dd55f6954f2598412fb375d17265ca6
RodionR-RR <radicrogogin9@gmail.com>
2026-01-25 08:12:13 -0800

    Обновление стилей

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,4 @@
 // src/components/header/Header.js
-import React from 'react';
 import { Link } from 'react-router-dom'; // для навигации
 import './Header.scss'; // добавляем точку с запятой
 
@@ -8,33 +7,39 @@ const Header = () => {
     <header className="header">
       <div className="header-content">
         <h1>ООО «КРЫМЖИЛКОМФОРТ»</h1>
-        <p>Строительство, реконструкция и реставрация объектов любой сложности</p>
+        <p>
+          Строительство, реконструкция и реставрация объектов любой сложности
+        </p>
       </div>
       <nav className="header-nav">
         <ul>
           <li>
             <div className="wrapper">
-              <Link to="/" className="button">Главная</Link>
+              <Link to="/" className="button">
+                Главная
+              </Link>
             </div>
           </li>
           <li>
             <div className="wrapper">
-              <Link to="/license" className="button">Лицензия</Link>
+              <Link to="/license" className="button">
+                Лицензия
+              </Link>
             </div>
           </li>
           <li>
             <div className="wrapper">
-              <Link to="/works" className="button">Наши работы</Link>
+              <Link to="/works" className="button">
+                Наши работы
+              </Link>
             </div>
           </li>
+
           <li>
             <div className="wrapper">
-              <Link to="/contact" className="button">Контакты</Link>
-            </div>
-          </li>
-          <li>
-            <div className="wrapper">
-              <Link to="/about" className="button">О нас</Link>
+              <Link to="/about" className="button">
+                О нас
+              </Link>
             </div>
           </li>
         </ul>
